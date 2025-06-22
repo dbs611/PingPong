893eb53d83adadb441d65c34c031d8f91130721b
Simon <dbs611@amumni.ku.dk>
2025-06-22 14:01:18 +0200

    Start on States

--- PingPong/Entities/Player.cs
+++ PingPong/Entities/Player.cs
@@ -7,9 +7,9 @@ using DIKUArcade.Input;
 using System.Numerics;
 
 public class player : Entity {
-    private float moveUp = 0.0f
-    private float moveDown = 0.0f
-    private const float BaseSpeed = 0.04f
+    private float moveUp = 0.0f;
+    private float moveDown = 0.0f;
+    private const float BaseSpeed = 0.04f;
 
     public player(DynamicShape shape, IBaseImage image) : base(shape, image) { }
 
@@ -17,6 +17,6 @@ public class player : Entity {
         DynamicShape dynamicshape = Shape.AsDynamicShape();
         DynamicShape.move();
 
-        float newY = Math.Clamp(ShapePosition.Y)
+        float newY = Math.Clamp(ShapePosition.Y);
     }
 }
---
+++ PingPong/States/GameRunning.cs
@@ -0,0 +1,27 @@
+namespace PingPong.States;
+
+using System;
+using System.Numerics;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.GUI;
+using DIKUArcade.Input;
+using DIKUArcade.Timers;
+using PingPong;
+using PingPong.Ball;
+using PingPong.Collisions;
+using Microsoft.VisualBasic;
+using PingPong.States;
+
+public class GameRunning : IGameState {
+    public void Update() {
+
+    }
+    public void Render() {
+
+    }
+    
+    public void HandleKeyevent() {
+
+    }
+}

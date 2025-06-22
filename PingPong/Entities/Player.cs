70e15b91ad50405b2bd864bcee7e90759ed38cd8
Simon <dbs611@amumni.ku.dk>
2025-06-22 13:49:45 +0200

    Ufærdigt Player class

---
+++ PingPong/Entities/Player.cs
@@ -0,0 +1,22 @@
+namespace PingPong.Entities;
+
+using System;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Input;
+using System.Numerics;
+
+public class player : Entity {
+    private float moveUp = 0.0f
+    private float moveDown = 0.0f
+    private const float BaseSpeed = 0.04f
+
+    public player(DynamicShape shape, IBaseImage image) : base(shape, image) { }
+
+    public move() {
+        DynamicShape dynamicshape = Shape.AsDynamicShape();
+        DynamicShape.move();
+
+        float newY = Math.Clamp(ShapePosition.Y)
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PingPong
 This is an offline multiplayer Ping Pong game 
+
+## To play:
+
+Download submodules for the program: <br>
+`git submodule update --init --recursive `
+
+Compile the project: <br>
+`dotnet build PingPon/PingPong.csproj`
+
+Run the program with: <br>
+`dotnet run -p PingPong`
